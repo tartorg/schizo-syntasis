3ac68ec69bad9aac20fd9deb81f7c0242fd9ce35
Th1nkK1D <witheep@gmail.com>
2020-06-12 15:18:29 +0700

    Add aframe with demo scene

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DemoScene />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DemoScene from '@/components/DemoScene.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    DemoScene,
   },
 }
 </script>
