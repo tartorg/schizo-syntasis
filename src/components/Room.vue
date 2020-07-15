6a7ebeef8e99576ff19a637627a401da95fa7c6e
Th1nkK1D <witheep@gmail.com>
2020-07-15 20:43:22 +0700

    Add Room component with gltf import

---
+++ src/components/Room.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-scene v-if="modelPath">
+    <a-entity :gltf-model="`url(${modelPath})`"></a-entity>
+  </a-scene>
+</template>
+
+<script>
+import { publicPath } from '../../vue.config'
+
+export default {
+  name: 'Room',
+  props: ['model'],
+  data() {
+    return {
+      modelPath: `${publicPath}/models/${this.model}`,
+    }
+  },
+}
+</script>
