c47f2978c80df66449957ef3f0fc2e353f1db05f
Th1nkK1D <witheep@gmail.com>
2020-09-08 12:23:20 +0700

    Use inline gltf import to improve performance

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,15 +1,8 @@
 <template>
-  <a-scene v-if="modelPath">
-    <a-assets @loaded="onModelLoaded">
-      <a-asset-item :id="path" :src="modelPath"></a-asset-item>
-    </a-assets>
-
-    <a-entity
-      v-if="isModelLoaded"
-      :gltf-model="`#${path}`"
-      :position="position.join(' ')"
-    ></a-entity>
-  </a-scene>
+  <a-entity
+    :gltf-model="`url(${modelPath})`"
+    :position="position.join(' ')"
+  ></a-entity>
 </template>
 
 <script>
@@ -21,13 +14,7 @@ export default {
   data() {
     return {
       modelPath: `${publicPath}/models/${this.path}`,
-      isModelLoaded: false,
     }
-  },
-  methods: {
-    onModelLoaded() {
-      this.isModelLoaded = true
-    },
   },
 }
 </script>
