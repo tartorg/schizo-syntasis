ec242dda82d285c8f8b52e44e0a14aba08fb9554
dodadoa <maddodadoa@gmail.com>
2021-08-04 13:47:36 +0700

    add input text

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,31 +1,70 @@
 <template>
-  <a-scene>
-    <a-assets>
-      <video
-        :id="backgroundVideo"
-        autoplay
-        loop
-        subtitles="backgroundVideo"
-        description="background video"
-        :src="videoPath"
-      ></video>
-    </a-assets>
+  <div>
+    <a-scene>
+      <a-assets>
+        <video
+          :id="backgroundVideo"
+          autoplay
+          loop
+          subtitles="backgroundVideo"
+          description="background video"
+          :src="videoPath"
+        ></video>
+      </a-assets>
 
-    <a-entity
-      v-for="(itemPath, index) in itemsPath"
-      :key="itemPath"
-      :gltf-model="itemPath"
-      :position="itemsPosition[index]"
-    ></a-entity>
+      <a-entity
+        v-for="(itemPath, index) in itemsPath"
+        :key="itemPath"
+        :gltf-model="itemPath"
+        :position="itemsPosition[index]"
+      ></a-entity>
 
-    <slot></slot>
+      <slot></slot>
 
-    <a-videosphere :src="`#${backgroundVideo}`"></a-videosphere>
-  </a-scene>
+      <a-videosphere :src="`#${backgroundVideo}`"></a-videosphere>
+    </a-scene>
+    <div class="outerInterface">
+      <input class="bigInput" />
+      <p class="questionsText">{{ questionsText }}</p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.outerInterface {
+  position: absolute;
+  width: 100%;
+  height: 0%;
+  display: flex;
+  bottom: 0;
+}
+
+.bigInput {
+  width: 100%;
+  height: 20px;
+  font-size: 20px;
+  padding: 20px;
+  margin: 30px;
+  margin-bottom: 10px;
+  align-self: flex-end;
+  background: transparent;
+  color: white;
+  border: 1px solid white;
+}
+.bigInput:focus {
+  outline-width: 0;
+}
+
+.questionsText {
+  align-self: flex-start;
+  width: 100%;
+  color: white;
+}
+</style>
 
 <script>
 import { publicPath } from '../../vue.config'
+import questionsText from '!raw-loader!../questions/list.txt'
 
 export default {
   name: 'Room',
@@ -51,6 +90,9 @@ export default {
     },
     videoPath() {
       return `${publicPath}/assets/${this.backgroundVideo}`
+    },
+    questions() {
+      return questionsText
     },
   },
 }
